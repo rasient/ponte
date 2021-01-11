052915924adf31c90ba11bd0838c33a1b1457db1
Alexander Berg <alexander.berg.job@gmail.com>
2021-01-11 14:03:51 +0100

    changed key.private loading

--- src/main/java/hu/ponte/hr/services/SignService.java
+++ src/main/java/hu/ponte/hr/services/SignService.java
@@ -1,8 +1,6 @@
 package hu.ponte.hr.services;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.security.InvalidKeyException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
@@ -12,13 +10,14 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Base64;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SignService {
 
 	public String digitalSign(byte[] file) throws IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidKeySpecException, SignatureException {
-		PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(Files.readAllBytes(Paths.get(("src\\main\\resources\\config\\keys\\key.private"))));
+		PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(IOUtils.toByteArray(this.getClass().getResourceAsStream("/config/keys/key.private")));
 		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         Signature signature = Signature.getInstance("SHA256withRSA");
         signature.initSign(keyFactory.generatePrivate(spec));
